14bc88c4a53c499e23dcc6e84718070f9de99455
andrew-ko <andrew.kovalenko16@gmail.com>
2016-08-08 23:00:36 +0300

    Steps 2,3

--- Psrlint/Rules/FunctionsNaming.php
+++ Psrlint/Rules/FunctionsNaming.php
@@ -2,54 +2,69 @@
 
 namespace Psrlint\Rules;
 
-use Psrlint\Interfaces\LintEngineInterface;
-use Psrlint\Interfaces\RuleInterface;
-use Psrlint\Ast;
-
-class FunctionsNaming implements RuleInterface
+class FunctionsNaming
 {
-    public $rules = [
+    public function init()
+    {
+        return [
+            'Stmt_Function' => function ($state, $payload) {
+                return $this->checkName($payload['node']);
+            },
+            'Stmt_ClassMethod' => function ($state, $payload) {
+                return $this->checkName($payload['node']);
+            }
+        ];
+    }
+
+    private function checkName($node)
+    {
+        $name = $node->name;
+
+        // ignore magic methods
+        if (in_array($name, $this->ignore)) {
+            return;
+        }
+
+        foreach ($this->rules as $rule) {
+            if (preg_match($rule['pattern'], $name)) {
+                return [
+                    'type' => $rule['type'],
+                    'line' => $node->getAttribute('startLine'),
+                    'message' => $rule['message']
+                ];
+            }
+        }
+    }
+
+    private $rules = [
         [
             'pattern' => '/^_/',
             'message' =>
-            'Method names SHOULD NOT be prefixed with a single underscore to indicate protected or private visibility.'
+            'Method names SHOULD NOT be prefixed with a single underscore to indicate protected or private visibility.',
+            'type' => 'warning'
         ],
         [
             'pattern' => '/^[A-Z]|[a-z][_]/',
-            'message' => 'Method names MUST be declared in camelCase().'
+            'message' => 'Method names MUST be declared in camelCase().',
+            'type' => 'error'
         ]
     ];
 
-    public function visit(LintEngineInterface $lintEngine)
-    {
-        $ast = $lintEngine->getAst();
-
-        $report = $this->processAst($ast);
-
-        $lintEngine->setReport($report);
-    }
-
-    protected function processAst(Ast $ast): array
-    {
-        $functionNodes = $ast->getFunctionNodes();
-
-        $report = [
-            'errors' => [],
-            'warnings' => [],
-            'fixes' => []
-        ];
-
-        foreach ($functionNodes as $node) {
-            foreach ($this->rules as $rule) {
-                if (preg_match($rule['pattern'], $node->name)) {
-                    $report['errors'][] = [
-                        'name' => $node->name,
-                        'message' => $rule['message']
-                    ];
-                }
-            }
-        }
-
-        return $report;
-    }
+    private $ignore = [
+        '__construct',
+        '__destruct',
+        '__call',
+        '__callstatic',
+        '__get',
+        '__set',
+        '__isset',
+        '__unset',
+        '__sleep',
+        '__wakeup',
+        '__tostring',
+        '__set_state',
+        '__clone',
+        '__invoke',
+        '__debuginfo',
+    ];
 }
--- tests/RulesTests/FunctionsNamingTest.php
+++ tests/RulesTests/FunctionsNamingTest.php
@@ -4,38 +4,53 @@ namespace Psrlint\Tests\RulesTests;
 
 use PHPUnit\Framework\TestCase;
 use Psrlint\Rules\FunctionsNaming;
-use Psrlint\LintEngine;
-use Psrlint\Ast;
+use PhpParser\Node\Stmt\ClassMethod;
+use PhpParser\Node\Stmt\Function_;
 
 class FunctionsNamingTest extends TestCase
 {
+
     protected function setUp()
     {
-        $this->ruleInstance = new FunctionsNaming();
+        $this->rule = new FunctionsNaming();
+        $this->listeners = $this->rule->init();
     }
 
-    public function testHaveRulesProperty()
+    public function testInit()
     {
-        $this->assertObjectHasAttribute('rules', new FunctionsNaming);
-    }
-
-    public function testRulesPropertyShape()
-    {
-        foreach ($this->ruleInstance->rules as $rule) {
-            $this->assertArrayHasKey('pattern', $rule);
-            $this->assertArrayHasKey('message', $rule);
+        foreach ($this->listeners as $listener) {
+            $this->assertTrue($listener instanceof \Closure);
         }
+        $this->assertArrayHasKey('Stmt_Function', $this->listeners);
+        $this->assertArrayHasKey('Stmt_ClassMethod', $this->listeners);
     }
 
-    public function testThatItWorksCorrectly()
+
+    public function testThatItWorks()
     {
-        $code = '<?php ' .
-            'function CamelCaps() {return;}' .
-            'function _Underscore() {return;}' .
-            'function lower_under() {return;}';
-        $ast = new Ast($code);
-        $lintEngine = new LintEngine($ast, [$this->ruleInstance], 'text');
-        $report = $lintEngine->process();
-        $this->assertEquals(3, count($report['sourceReport']['errors']));
+        $nodes[] = new ClassMethod('CamelCaps');
+        $nodes[] = new ClassMethod('_Underscore');
+        $nodes[] = new ClassMethod('snake_case');
+        $nodes[] = new ClassMethod('camelCase');
+        $nodes[] = new Function_('CamelCaps');
+        $nodes[] = new Function_('_Underscore');
+        $nodes[] = new Function_('snake_case');
+        $nodes[] = new Function_('camelCase');
+
+        foreach ($nodes as $node) {
+            $results[] = $this->listeners[$node->getType()]([], ['node' => $node]);
+        }
+
+        $warnings = array_filter($results, function ($el) {
+            return $el['type'] === 'warning';
+        });
+
+        $errors = array_filter($results, function ($el) {
+            return $el['type'] === 'error';
+        });
+
+        $this->assertEquals(8, count($results));
+        $this->assertEquals(2, count($warnings));
+        $this->assertEquals(4, count($errors));
     }
 }
